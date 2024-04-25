9c758cddb6707451eae64ac14337b728c87d35d4
Shrouk2004 <154844049+Shrouk2004@users.noreply.github.com>
2024-04-25 22:37:38 +0200

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -1,5 +1,6 @@
 #include "headers.h"
-
+//--------------------------------------------------------------------------------------------------
+int ALGORITHM;
 
 int main(int argc, char * argv[])
 {
@@ -7,6 +8,16 @@ int main(int argc, char * argv[])
     
     //TODO implement the scheduler :)
     //upon termination release the clock resources.
-    
+     struct msgbuff Msg_Snd;
+     ALGORITHM = atoi(argv[1]);
+     int key_id;
+     int rec_val, msgq_id;
+     key_id = ftok("keyfile", 65);     // CREATE A UNIQUE KEY
+     msgq_id = msgget(key_id, 0666 | IPC_CREAT); // CREATE MESSAGE QUEUE THEN RETURN THE ID
+    if (msgq_id == -1)
+    {
+        perror("Error in create");
+        exit(-1);
+    }
     destroyClk(true);
 }
