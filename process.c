56f13496b175b55f2731eac6165caace0e0960cc
Shrouk2004 <154844049+Shrouk2004@users.noreply.github.com>
2024-04-30 05:42:47 +0300

    Update process.c

--- process.c
+++ process.c
@@ -1,20 +1,38 @@
 #include "headers.h"
+struct scheduler_process_buff {
+    
+    int get_remaining_time[2];
+    int m_type;
+};
 
-/* Modify this file as needed*/
 int remainingtime;
 
-int main(int agrc, char * argv[])
+int main(int argc, char *argv[])
 {
     initClk();
-    
-    //TODO it needs to get the remaining time from somewhere
-    //remainingtime = ??;
-    while (remainingtime > 0)
+
+    remainingtime = atoi(argv[2]); // Set remaining time from command line argument
+
+    key_t CLK_key_id, CLK_msgq_id;
+    int CLK_rec_val;
+    struct scheduler_process_buff CLK_buf;
+    CLK_buf.m_type = 1;
+
+    CLK_key_id = ftok("keyfile", 63);                   // create unique key for clock
+    CLK_msgq_id = msgget(CLK_key_id, 0666 | IPC_CREAT); // create message queue for clock
+
+    while (remainingtime > 0) 
     {
-        // remainingtime = ??;
+        CLK_rec_val = msgrcv(CLK_msgq_id, &CLK_buf, sizeof(&CLK_buf.get_remaining_time), 0, IPC_NOWAIT);
+
+        if (CLK_rec_val != -1)
+        {
+            remainingtime -= 1; // Decrement remaining time when a clock tick is received
+        }
     }
-    
-    destroyClk(false);
-    
+
+    destroyClk(false); // Cleanup clock resources
+    // Notify the scheduler on termination
+    kill(getppid(), SIGUSR1); // Send SIGUSR1 signal to scheduler
     return 0;
 }
