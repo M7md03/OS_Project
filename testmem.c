33badb88555c2dec778eabfc2ed5899e7bdde185
M7md03 <tifa1862003@gmail.com>
2024-05-09 22:58:20 +0300

    .

--- testmem.c
+++ testmem.c
@@ -1,88 +1,66 @@
-#include "memory.h"
-#include "memory.c"
-int main(void)
-{
+#include "headers.h"
+
+int main(void) {
     struct MemoryNode *root = createMemoryNode(MaxSize, NULL, 0);
-    struct MemoryNode *nodeA = allocate(70,root);
-    //process.Mem=NodeA;
+    struct MemoryNode *nodeA = allocate(70, root);
+    // process.Mem=NodeA;
     PreorderTraverse(root);
-    //first allocation correct 
-    if(nodeA != NULL)
-    {
+    // first allocation correct
+    if (nodeA != NULL) {
         printf("allocated Memory %d \n", nodeA->size);
-        printf("starting position %d  and end position %d \n", nodeA ->block.start_address, nodeA->block.end_address);
-    }
-    else
-    {
+        printf("starting position %d  and end position %d \n", nodeA->block.start_address, nodeA->block.end_address);
+    } else {
         printf("error\n");
     }
-    //second allocation
-    PreorderTraverse(root); 
+    // second allocation
+    PreorderTraverse(root);
     struct MemoryNode *nodeB = allocate(35, root);
-    if(nodeB != NULL)
-    {
+    if (nodeB != NULL) {
         printf("allocated Memory %d \n", nodeB->size);
-        printf("starting position %d  and end position %d \n", nodeB ->block.start_address, nodeB->block.end_address);
-    }
-    else
-    {
+        printf("starting position %d  and end position %d \n", nodeB->block.start_address, nodeB->block.end_address);
+    } else {
         printf("error\n");
     }
     PreorderTraverse(root);
-    struct MemoryNode *nodeC =allocate(80, root);
-    //3RD allocation 
-    if(nodeC != NULL)
-    {
+    struct MemoryNode *nodeC = allocate(80, root);
+    // 3RD allocation
+    if (nodeC != NULL) {
         printf("allocated Memory %d \n", nodeC->size);
-        printf("starting position %d  and end position %d \n", nodeC ->block.start_address, nodeC->block.end_address);
-    }
-    else
-    {
+        printf("starting position %d  and end position %d \n", nodeC->block.start_address, nodeC->block.end_address);
+    } else {
         printf("error\n");
     }
     deallocateMemory(nodeA);
     PreorderTraverse(root);
     struct MemoryNode *nodeD = allocate(60, root);
-    if(nodeD != NULL)
-    {
+    if (nodeD != NULL) {
         printf("allocated Memory %d \n", nodeD->size);
         printf("starting position %d  and end position %d \n", nodeD->block.start_address, nodeD->block.end_address);
-    }
-    else
-    {
+    } else {
         printf("error\n");
     }
     PreorderTraverse(root);
     struct MemoryNode *nodeE = allocate(500, root);
-    if(nodeE != NULL)
-    {
+    if (nodeE != NULL) {
         printf("allocated Memory %d \n", nodeE->size);
         printf("starting position %d  and end position %d \n", nodeE->block.start_address, nodeE->block.end_address);
-    }
-    else
-    {
+    } else {
         printf("error\n");
     }
     PreorderTraverse(root);
     struct MemoryNode *nodeF = allocate(100, root);
-    if(nodeF != NULL)
-    {
+    if (nodeF != NULL) {
         printf("allocated Memory %d \n", nodeF->size);
         printf("starting position %d  and end position %d \n", nodeF->block.start_address, nodeF->block.end_address);
-    }
-    else
-    {
+    } else {
         printf("error\n");
     }
     PreorderTraverse(root);
     struct MemoryNode *nodeG = allocate(100, root);
-    if(nodeG != NULL)
-    {
+    if (nodeG != NULL) {
         printf("allocated Memory %d \n", nodeG->size);
         printf("starting position %d  and end position %d \n", nodeG->block.start_address, nodeG->block.end_address);
-    }
-    else
-    {
+    } else {
         printf("error couldn't find enough space\n");
     }
     PreorderTraverse(root);
@@ -101,13 +79,10 @@ int main(void)
     printf("deallocated G\n");
     PreorderTraverse(root);
     struct MemoryNode *nodek = allocate(100, root);
-    if(nodek != NULL)
-    {
+    if (nodek != NULL) {
         printf("allocated Memory %d \n", nodek->size);
         printf("starting position %d  and end position %d \n", nodek->block.start_address, nodek->block.end_address);
-    }
-    else
-    {
+    } else {
         printf("error couldn't find enough space\n");
     }
     return 0;
